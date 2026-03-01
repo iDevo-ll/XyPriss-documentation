6e9e66874a5cb555638df3851cd003576a59b85c
loan <loankl1728@gmail.com>
2026-03-01 14:30:41 +0000

    updated xfpm install script

--- public/install.js
+++ public/install.js
@@ -28,7 +28,7 @@ const COLORS = {
   magenta: "\x1b[35m",
 };
 
-const BASE_URL = "https://dll.nehonix.com/repo/n/fractostate/scripts";
+const BASE_URL = "https://dll.nehonix.com/repo/n/xypriss/xfpm/scripts";
 
 function log(msg, color = COLORS.reset) {
   console.log(`${color}${msg}${COLORS.reset}`);
